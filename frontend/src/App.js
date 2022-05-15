dd21ec7efc87182474f52556b3157500552549e8
Ritwik <shawritwik2@gmail.com>
2022-05-15 20:38:37 +0530

    Front End User Authentication and Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Footer from "./components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
 import CartScreen from "./Screens/CartScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
 
 const App = () => {
   return (
@@ -12,9 +15,12 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route exact path="/" component={HomeScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-// import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -22,9 +33,30 @@ const Header = () => {
               <Nav.Link as={Link} to="/cart">
                 <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
-              <Nav.Link as={Link} to="/login">
-                <i className="fas fa-user"></i> SignIn
-              </Nav.Link>
+
+              {userInfo ? (
+                // <NavDropdown title={userInfo.name} id="username">
+                //   <Nav.Link as={Link} to="/profile">
+                //     <NavDropdown.Item>Profile</NavDropdown.Item>
+                //   </Nav.Link>
+
+                //   <NavDropdown.Item onClick={logoutHandler}>
+                //     Logout
+                //   </NavDropdown.Item>
+                // </NavDropdown>
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to="/login">
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
