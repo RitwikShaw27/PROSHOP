3a9a363ce1da2da44aeafd6a8af08d547dd075c9
Ritwik <shawritwik2@gmail.com>
2022-06-05 13:36:31 +0530

    Pagination feature added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,10 +35,25 @@ const App = () => {
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-          <Route path="/admin/productlist" component={ProductListScreen} />
+          <Route
+            exact
+            path="/admin/productlist"
+            component={ProductListScreen}
+          />
+          <Route
+            exact
+            path="/admin/productlist/:pageNumber"
+            component={ProductListScreen}
+          />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
-          <Route path="/search/:keyword" component={HomeScreen} />
+          <Route exact path="/search/:keyword" component={HomeScreen} />
+          <Route exact path="/page/:pageNumber" component={HomeScreen} />
+          <Route
+            exact
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+          />
           <Route exact path="/" component={HomeScreen} />
         </Container>
       </main>
